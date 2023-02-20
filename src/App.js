babb25de7ebb562ec0f8205763ae93515880c436
Kirilll1 <kiryamishatkin@icloud.com>
2023-02-20 15:11:53 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Todolist from './TodoList.js/TodoList';
+
+function App() {
+  return (
+    <><div id="container"></div>
+    <script src="https://mapgl.2gis.com/api/js/v1"></script>
+    <script src="https://unpkg.com/@2gis/mapgl-directions@^2/dist/directions.js"></script></>
+    
+  );
+  
+}
+
+export default App;
